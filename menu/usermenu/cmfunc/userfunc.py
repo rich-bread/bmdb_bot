3a7b9c3b28b647ebbfd720d93f14afa34cb9b9ef
RichBread <royal.richbread@gmail.com>
2023-01-27 16:00:54 +0900

    delete unuse codes

--- menu/usermenu/cmfunc/userfunc.py
+++ menu/usermenu/cmfunc/userfunc.py
@@ -18,12 +18,6 @@ def check_userdata(user:discord.User) -> list:
     else: return ud
 
 #入力されたフレンドコードの確認
-def old_check_friendcode(friendcode:int) -> bool:
-    d = 12 #受付桁数
-    if friendcode < 0 or len(str(friendcode)) != d: return False
-    else: return True
-
-#入力されたフレンドコードの確認
 def check_friendcode(friendcode:str) -> bool:
     r = re.search(r"\d{4}-\d{4}-\d{4}",friendcode)
     return bool(r)
@@ -37,5 +31,3 @@ def check_twitterid(twitterid:str) -> bool:
 def apply_userdata(author:discord.User, user:discord.User, apptype:int, userdata:dict) -> requests.Response:
     r = database.Database().post_db(name='apply', data=userdata, tablename='indiv-master', apptype=apptype, authorname=str(author), authorid=str(author.id), subjectid=str(user.id))
     return r
-
-print(check_twitterid("1111-2222-3333"))
